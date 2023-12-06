a17a94f6dfec69e40bd99d36b7afe3bf6ec71400
alachihi <alachihi464@gmail.com>
2023-12-06 18:53:09 +0100

    lets go

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -60,8 +60,8 @@ app.use((req, res, next) => {
 
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
   console.log(error);
-  // let errorMessage = "An unknown error occurred";
-  // let statusCode = 500;
+  let errorMessage = "An unknown error occurred";
+  let statusCode = 500;
   if (isHttpError(error)) {
     statusCode = error.status;
     errorMessage = error.message;
