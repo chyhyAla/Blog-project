c97ac93d5c6dc2de8bfe7bd4756920f321fa1c71
alachihi <alachihi464@gmail.com>
2023-12-06 09:17:24 +0100

    initial

--- frontend/src/network/notes_api.ts
+++ frontend/src/network/notes_api.ts
@@ -2,30 +2,37 @@ import { ConflictError, UnauthorizedError } from "../errors/http_errors";
 import { Note } from "../models/note";
 
 const baseUrl = "https://notes-app-api-m6rv.onrender.com";
+
 async function fetchData(input: RequestInfo, init?: RequestInit) {
   const response = await fetch(`${baseUrl}${input}`, init);
 
-  if (response.ok) {
-    return response;
-  } else {
-    const errorBody = await response.json();
-    const errorMessage = errorBody.error;
+  try {
+    const data = await response.json();
 
-    if (response.status === 401) {
-      throw new UnauthorizedError(errorMessage);
-    } else if (response.status === 409) {
-      throw new ConflictError(errorMessage);
-    } else {
-      throw Error(
-        `Request failed with status ${response.status}, message: ${errorMessage}`
-      );
+    if (!response.ok) {
+      console.error("Response status:", response.status);
+      console.error("Error message:", data.error);
+
+      if (response.status === 401) {
+        throw new UnauthorizedError(data.error);
+      } else if (response.status === 409) {
+        throw new ConflictError(data.error);
+      } else {
+        throw Error(
+          `Request failed with status ${response.status}, message: ${data.error}`
+        );
+      }
     }
+
+    return data;
+  } catch (error) {
+    console.error("Error parsing response:", error);
+    throw Error("Failed to parse response");
   }
 }
 
 export async function fetchNotes(): Promise<Note[]> {
-  const response = await fetchData("/api/notes", { method: "GET" });
-  return response.json();
+  return fetchData("/api/notes", { method: "GET" });
 }
 
 export interface Noteinput {
@@ -34,29 +41,28 @@ export interface Noteinput {
 }
 
 export async function createNote(note: Noteinput): Promise<Note> {
-  const response = await fetchData("/api/notes", {
+  return fetchData("/api/notes", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(note),
   });
-  return response.json();
 }
 
 export async function deleteNote(noteId: string) {
-  await fetchData("/api/notes/" + noteId, { method: "DELETE" });
+  return fetchData("/api/notes/" + noteId, { method: "DELETE" });
 }
+
 export async function updateNote(
   noteId: string,
   note: Noteinput
 ): Promise<Note> {
-  const response = await fetchData("/api/notes/" + noteId, {
+  return fetchData("/api/notes/" + noteId, {
     method: "PATCH",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(note),
   });
-  return response.json();
 }
