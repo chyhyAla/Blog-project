06d327e1d55e56cef236dfa978eb82310e9abb68
alachihi <alachihi464@gmail.com>
2023-12-06 08:12:41 +0100

    initial

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -43,16 +43,16 @@ app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/api/notes", notesRoutes);
 app.use("/api/users", usersRoutes);
 
-app.all("*", (req, res) => {
-  res.status(404);
-  if (req.accepts("html")) {
-    res.sendFile(path.join(__dirname, "views", "404.html"));
-  } else if (req.accepts("json")) {
-    res.json({ message: "404 Not Found" });
-  } else {
-    res.type("txt").send("404 Not Found");
-  }
-});
+// app.all("*", (req, res) => {
+//   res.status(404);
+//   if (req.accepts("html")) {
+//     res.sendFile(path.join(__dirname, "views", "404.html"));
+//   } else if (req.accepts("json")) {
+//     res.json({ message: "404 Not Found" });
+//   } else {
+//     res.type("txt").send("404 Not Found");
+//   }
+// });
 
 app.use((req, res, next) => {
   next(createHttpError(404, "Endpoint not found"));
