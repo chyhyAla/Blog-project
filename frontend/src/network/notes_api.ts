ff90dcfb97d43eeba4777a23045c9dceba4205ad
alachihi <alachihi464@gmail.com>
2023-12-06 19:07:57 +0100

    lets go

--- frontend/src/network/notes_api.ts
+++ frontend/src/network/notes_api.ts
@@ -27,7 +27,7 @@ async function fetchData(input: RequestInfo, init?: RequestInit) {
     return data;
   } catch (error) {
     console.error("Error parsing response:", error);
-    console.error("Raw response:", response);
+    console.error("Raw response:", response.statusText, response.headers);
     throw Error("Failed to parse response");
   }
 }
