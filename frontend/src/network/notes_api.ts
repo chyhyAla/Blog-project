21ddd5664c7f1d128b0db991823fd20658c0c1a6
alachihi <alachihi464@gmail.com>
2023-12-08 18:16:40 +0100

    we go again

--- frontend/src/network/notes_api.ts
+++ frontend/src/network/notes_api.ts
@@ -74,6 +74,7 @@ export async function login(credentials: LoginCredentials): Promise<User> {
 
   // console.log("test");
   const user = await response.json();
+  console.log(user);
   return user;
 }
 
