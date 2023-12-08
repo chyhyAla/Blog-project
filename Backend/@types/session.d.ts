f947933d685e2d94993974e1a5cf9bdca95edd0e
alachihi <alachihi464@gmail.com>
2023-12-08 20:58:09 +0100

    one

--- Backend/@types/session.d.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import { SessionData } from "express-session";
-
-declare module "express session" {
-  interface SessionData {
-    userId: mongoose.Types.ObjectId;
-  }
-}
