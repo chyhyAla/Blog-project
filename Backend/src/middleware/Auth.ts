38a4f08fa90a56ba2c2da94c158f82f67fb3e6f5
alachihi <alachihi464@gmail.com>
2023-12-08 16:51:39 +0100

    here we go

--- Backend/src/middleware/Auth.ts
+++ Backend/src/middleware/Auth.ts
@@ -3,6 +3,7 @@ import createHttpError from "http-errors";
 
 export const requireAuth: RequestHandler = (req, res, next) => {
   if (req.session.userId) {
+    console.log(req.session);
     next();
   } else {
     next(createHttpError(401, "User not authenticated"));
