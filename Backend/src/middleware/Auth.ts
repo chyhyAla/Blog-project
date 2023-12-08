0349606ea63159a0f66c3b41311530d394e7eb47
alachihi <alachihi464@gmail.com>
2023-12-08 22:50:03 +0100

    one

--- Backend/src/middleware/Auth.ts
+++ Backend/src/middleware/Auth.ts
@@ -4,6 +4,7 @@ import createHttpError from "http-errors";
 // In your Auth middleware
 export const requireAuth: RequestHandler = (req, res, next) => {
   const user = req.session.userId;
+  console.log(req.session);
   if (user) {
     console.log("User is authenticated. UserId:", req.session.userId);
     next();
