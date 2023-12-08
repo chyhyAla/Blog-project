899e84f9eb21d2beb0a310d86826fff48c87b292
alachihi <alachihi464@gmail.com>
2023-12-08 21:16:59 +0100

    one

--- Backend/src/routes/users.ts
+++ Backend/src/routes/users.ts
@@ -5,7 +5,7 @@ import { requireAuth } from "../middleware/Auth";
 const router = express.Router();
 router.get("/", requireAuth, userController.getAuthenticatedUser);
 router.post("/Signup", userController.SignUp);
-router.post("/Login", userController.Login);
+router.post("/Login", requireAuth, userController.Login);
 
 router.post("/Logout", userController.Logout);
 
