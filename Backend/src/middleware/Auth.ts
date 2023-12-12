074c1c88cbf0fbaddc46e6bfab0c06e26cd70221
alachihi <alachihi464@gmail.com>
2023-12-12 20:26:13 +0100

    we go again

--- Backend/src/middleware/Auth.ts
+++ Backend/src/middleware/Auth.ts
@@ -6,9 +6,9 @@ import createHttpError from "http-errors";
 export const requireAuth: RequestHandler = (req, res, next) => {
   console.log("Headers:", req.headers);
   console.log("Session:", req.session);
-  console.log("Cookies:", req.cookies);
+  // console.log("Cookies:", req.cookies);
 
-  const userID = req.cookies.userId;
+  const userID = req.session.userId;
 
   // console.log("User:", user);
 
