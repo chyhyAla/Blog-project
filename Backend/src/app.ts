da51d558bfb612102ad0aa15b61d9d22564cba07
alachihi <alachihi464@gmail.com>
2023-12-12 11:58:03 +0100

    we go

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -57,12 +57,12 @@ app.use(
 // app.use("/api/users", usersRoutes);
 // app.use("/api/notes", requireAuth, notesRoutes);
 
-router.post("/api/users/signup", UserController.SignUp);
+app.post("/api/users/signup", UserController.SignUp);
 
-router.post("/api/users/login", UserController.Login);
-router.get("/api/users/", requireAuth, UserController.getAuthenticatedUser);
+app.post("/api/users/login", UserController.Login);
+app.get("/api/users/", requireAuth, UserController.getAuthenticatedUser);
 
-router.post("api/users/logout", UserController.Logout);
+app.post("api/users/logout", UserController.Logout);
 
 // app.all("*", (req, res) => {
 //   res.status(404);
