b3bac70dc6adc5848180244442b064826dcad363
alachihi <alachihi464@gmail.com>
2023-12-12 11:42:48 +0100

    test

--- Backend/src/routes/users.ts
+++ Backend/src/routes/users.ts
@@ -1,15 +1,15 @@
-import express from "express";
-import * as UserController from "../controllers/users";
-import { requireAuth } from "../middleware/Auth";
+// import express from "express";
+// import * as UserController from "../controllers/users";
+// import { requireAuth } from "../middleware/Auth";
 
-const router = express.Router();
+// const router = express.Router();
 
-router.get("/", requireAuth, UserController.getAuthenticatedUser);
+// router.get("/", requireAuth, UserController.getAuthenticatedUser);
 
-router.post("/signup", UserController.SignUp);
+// router.post("/signup", UserController.SignUp);
 
-router.post("/login", UserController.Login);
+// router.post("/login", UserController.Login);
 
-router.post("/logout", UserController.Logout);
+// router.post("/logout", UserController.Logout);
 
-export default router;
+// export default router;
